ec35a08776f977e27357e474c3f96b37d338749c
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-08-23 17:37:14 -0500

    feat: added utility function to animate element using WAAPI

--- liamazons/src/games/amazons/util.ts
+++ liamazons/src/games/amazons/util.ts
@@ -1,11 +1,27 @@
 import { square_to_coords } from "amazons-game-engine";
-import { Square } from "amazons-game-engine/dist/types";
+import { Size, Square } from "amazons-game-engine/dist/types";
 
-export function makeTransform(sq: Square) {
-  const { row, col } = square_to_coords(sq, { rows: 6, cols: 6 });
+export function makeTransform(sq: Square, size: Size) {
+  const { row, col } = square_to_coords(sq, size);
 
   const ans = `translate3d(${col === 0 ? "0" : col + "00%"}, ${
     row === 0 ? "0" : row + "00%"
   }, 0)`;
   return ans;
+}
+
+export function makeAndRunAnim(el: HTMLElement, sq: Square, size: Size) {
+  const transformStr = makeTransform(sq, size);
+
+  el.animate(
+    {
+      transform: transformStr,
+    },
+    {
+      duration: 250,
+      easing: "ease-in-out",
+    }
+  ).onfinish = () => {
+    el.style.transform = transformStr;
+  };
 }
