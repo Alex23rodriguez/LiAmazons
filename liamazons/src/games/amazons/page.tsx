5b00107e396e93390934f94a32a6460396b383be
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-08-23 19:48:26 -0500

    feat: added Page component, to possibly create Context there

---
+++ liamazons/src/games/amazons/page.tsx
@@ -0,0 +1,18 @@
+import { BoardProps } from "boardgame.io/dist/types/packages/react";
+import { createContext, FC } from "react";
+import { Board } from "./board";
+import { AmazonsState } from "./game";
+
+export const GameContext = createContext(null);
+
+export const Page: FC<BoardProps<AmazonsState>> = (props) => {
+  return (
+    <>
+      <Board {...props} />
+      {/*
+      <Settings /> 
+      <Chat />
+      */}
+    </>
+  );
+};
