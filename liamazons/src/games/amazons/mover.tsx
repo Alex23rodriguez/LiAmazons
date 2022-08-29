a0fc192cc882d543728c5594a4b8054ae6b80654
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-08-29 13:42:11 -0500

    feat: added mover.tsx

---
+++ liamazons/src/games/amazons/mover.tsx
@@ -0,0 +1,17 @@
+import { Square as TSquare } from "amazons-game-engine/dist/types";
+import { FC } from "react";
+import { colorPalette } from "./settings";
+export const Mover: FC<{ shooting: boolean }> = ({ shooting }) => {
+  return (
+    <div className="absolute grid place-items-center w-full h-full">
+      <div
+        className="rounded-full w-1/4 h-1/4 z-10"
+        style={{
+          opacity: 0.5,
+          backgroundColor:
+            colorPalette[shooting ? "canMoveShooting" : "canMove"],
+        }}
+      ></div>
+    </div>
+  );
+};
