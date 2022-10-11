a21d450924dd003a76d8b271bea1fe371728cdc8
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-10-11 18:15:32 -0500

    feat: added gif to index page to showcase gameplay

--- liamazons/src/pages/index.tsx
+++ liamazons/src/pages/index.tsx
@@ -2,7 +2,9 @@ import { Header } from "../components/Header";
 import type { NextPage } from "next";
 // import Head from "next/head";
 import Link from "next/link";
-
+import Image from "next/image";
+import gif from "../assets/anim.gif";
+import { Typography } from "@mui/material";
 // import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
@@ -10,11 +12,27 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <main className="container flex flex-col items-center justify-between min-h-[50vh] p-6 mx-auto">
-        <span className="font-bold text-6xl text-center">liamazons</span>
-        <span className="font-bold text-4xl text-center">
+      <main
+        className="container flex flex-col items-center justify-around p-6 mx-auto text-center font-bold"
+        style={{ minHeight: "clamp(550px, 90vh, 10000px)" }}
+      >
+        <Typography variant="h2" color="primary">
+          liamazons
+        </Typography>
+        {/* <span className="font-bold text-6xl text-center">/Typography></span> */}
+        <Typography variant="h4" text-center>
           The best way to play Game of the Amazons
-        </span>
+        </Typography>
+        {/* <span className="font-bold text-4xl text-center"> */}
+        {/* The best way to play Game of the Amazons */}
+        {/* </span> */}
+        <div
+          style={{
+            width: "clamp(300px, 80vw, 500px)",
+          }}
+        >
+          <Image src={gif} />
+        </div>
         <Link href="/amazons">
           <a>Play Locally</a>
         </Link>
