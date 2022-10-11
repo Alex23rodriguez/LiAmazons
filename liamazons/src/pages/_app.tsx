fad0adac44d669bf3ff2c08daebe471414311886
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-10-11 10:51:29 -0500

    feat: added theme provider to application

--- liamazons/src/pages/_app.tsx
+++ liamazons/src/pages/_app.tsx
@@ -5,10 +5,35 @@ import type { AppType } from "next/dist/shared/lib/utils";
 import { trpc } from "../utils/trpc";
 import "../styles/lobby.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+import createEmotionCache from "../utils/createEmotionCache";
+import { useState } from "react";
+
+const MyApp: AppType = (props) => {
+  const [myTheme, setMyTheme] = useState<"dark" | "light">("light");
+  const theme = createTheme({
+    palette: {
+      mode: myTheme,
+    },
+  });
+
+  const clientSideEmotionCache = createEmotionCache();
+
+  const {
+    Component,
+    emotionCache = clientSideEmotionCache,
+    pageProps,
+  } = props as any;
+
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </SessionProvider>
   );
 };
