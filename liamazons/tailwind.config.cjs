1845e9e19697fa531546233ba1f2bcc8da4effb8
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-10-12 17:51:45 -0500

    fix: apparently, reverting to what was previously there (doing nothing) fixes button style bug!

--- liamazons/tailwind.config.cjs
+++ liamazons/tailwind.config.cjs
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // important: "#__next",
+  corePlugins: {
+    // preflight: false,
+  },
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
