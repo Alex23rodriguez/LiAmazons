c57ce858e0959f1683fbca48f472682e2698d9fa
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-10-13 12:41:42 -0500

    refac: moved favicon to _document as it should be

--- liamazons/src/pages/_app.tsx
+++ liamazons/src/pages/_app.tsx
@@ -5,18 +5,16 @@ import type { AppType } from "next/dist/shared/lib/utils";
 import { trpc } from "../utils/trpc";
 import "../styles/lobby.css";
 
-import Head from "next/head";
-
 import { CacheProvider } from "@emotion/react";
-import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import createEmotionCache from "../utils/createEmotionCache";
 import { useState } from "react";
 import { Header } from "../components/Header";
-import {darkTheme, lightTheme} from '../utils/themes'
+import { darkTheme, lightTheme } from "../utils/themes";
 
 const MyApp: AppType = (props) => {
   const [myTheme, setMyTheme] = useState<"dark" | "light">("dark");
-  const theme = myTheme === 'dark' ? darkTheme : lightTheme
+  const theme = myTheme === "dark" ? darkTheme : lightTheme;
 
   const clientSideEmotionCache = createEmotionCache();
 
@@ -31,15 +29,6 @@ const MyApp: AppType = (props) => {
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Head>
-            <title>liamazons</title>
-            <meta name="description" content="liamazons" />
-            <link rel="icon" href="/favicon.svg" />
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,700&display=swap"
-            />
-          </Head>
           <Header
             toggleTheme={() => {
               setMyTheme(myTheme === "dark" ? "light" : "dark");
