9d7f4502c8877358c1249fa6405eb371a7460b4f
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-10-20 11:19:41 -0500

    feat: added multiple miniboard previews

--- src/components/CreateGame.tsx
+++ src/components/CreateGame.tsx
@@ -1,7 +1,21 @@
 import { Button, ButtonGroup, Divider, Stack, Typography } from "@mui/material";
+import { useState } from "react";
 import { MiniBoard } from "./MiniBoard";
 
+const boardWidth = "250px";
+
+const boards = [
+  <MiniBoard layout="3b2/6/b5/5w/6/2w3" width={boardWidth} />,
+  <MiniBoard layout="3b4/8/b6b/8/8/w6w/8/4w3" width={boardWidth} />,
+  <MiniBoard
+    layout="3b2b3/10/10/b8b/10/10/w8w/10/10/3w2w3"
+    width={boardWidth}
+  />,
+];
+
 export const CreateGame = () => {
+  const [index, changeIndex] = useState(0);
+
   return (
     <Stack alignItems="center" spacing={2}>
       <Typography>Board</Typography>
@@ -11,12 +25,20 @@ export const CreateGame = () => {
         size="large"
         sx={{ width: "100%" }}
       >
-        <Button sx={{ width: "25%" }}>6x6</Button>
-        <Button sx={{ width: "25%" }}>8x8</Button>
-        <Button sx={{ width: "25%" }}>10x10</Button>
-        <Button sx={{ width: "25%" }}>Custom</Button>
+        <Button onClick={() => changeIndex(0)} sx={{ width: "25%" }}>
+          6x6
+        </Button>
+        <Button onClick={() => changeIndex(1)} sx={{ width: "25%" }}>
+          8x8
+        </Button>
+        <Button onClick={() => changeIndex(2)} sx={{ width: "25%" }}>
+          10x10
+        </Button>
+        <Button onClick={() => changeIndex(0)} sx={{ width: "25%" }}>
+          Custom
+        </Button>
       </ButtonGroup>
-      <MiniBoard layout={"3b2/6/b5/5w/6/2w3"} width="200px" />
+      {boards[index]}
       <Divider />
       <Typography>Time control</Typography>
       <ButtonGroup
