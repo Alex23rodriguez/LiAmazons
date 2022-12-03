fc478256f13156d349e5099ad4a7bdc1c0de4c8e
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-12-02 21:40:03 -0600

    fix: removed unused component

--- src/games/amazons/layouts/singleplayer.tsx
+++ src/games/amazons/layouts/singleplayer.tsx
@@ -2,22 +2,10 @@ import { BoardProps } from "boardgame.io/dist/types/packages/react";
 import { createContext, FC } from "react";
 // import { Board } from "./board";
 import { Board } from "../board";
-import Chat from "../chat";
 import { AmazonsState } from "../game";
 
 export const GameContext = createContext(null);
 
 export const Layout: FC<BoardProps<AmazonsState>> = (props) => {
-  /* console.log(props.ctx) */
-  return (
-    <>
-      <Board {...props} />
-      {props.isMultiplayer && props.playerID !== null && (
-        <>
-          <div style={{ marginTop: "1rem" }} />
-          <Chat onSend={props.sendChatMessage} messages={props.chatMessages} />
-        </>
-      )}
-    </>
-  );
+  return <Board {...props} />;
 };
